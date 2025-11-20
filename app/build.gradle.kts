521481e38cae6b4a0f0d22ebfdd6f03ba1699b67
Sebas <sebastian.terrazas@ucb.edu.bo>
2025-11-19 22:00:59 -0400

    se agrego LOCO automatizado, ademas de acceso a localproperties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,24 @@
+import com.google.protobuf.gradle.id
+import java.io.File
+import java.net.URL
+import java.util.Properties
+
+import java.io.FileInputStream
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlinSerialization)
+
+    alias(libs.plugins.google.protobuf)
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 android {
@@ -16,8 +31,10 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val supabaseKey = localProperties["SUPABASE_KEY"] ?: ""
+        buildConfigField("String", "SUPABASE_KEY", "\"${localProperties["SUPABASE_KEY"]}\"")
+
     }
 
     buildTypes {
@@ -38,8 +55,74 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
+    }
+
+}
+
+fun getLocalProperty(key: String): String {
+    val localProperties = Properties()
+    val localPropertiesFile = project.rootProject.file("local.properties")
+
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { input ->
+            localProperties.load(input)
+        }
+    } else {
+        error("Error: El archivo 'local.properties' no se encuentra en la raíz del proyecto. Asegúrese de crearlo.")
+    }
+    return localProperties.getProperty(key) ?: error("Error: La clave '$key' no se encuentra en 'local.properties'. Por favor, añádala con su valor.")
+}
+val localeMapping = mapOf(
+    "en-US" to "values",
+    "es-ES" to "values-es",
+    "es-BO" to "values-es-rBO"
+)
+tasks.register("downloadLocoStrings") {
+    group = "localization"
+    description = "Downloads strings.xml files from Localise.biz via API"
+
+    val locoApiKey = getLocalProperty("LOCO_API_KEY")
+
+    val resDir = file("src/main/res")
+
+    doLast {
+        localeMapping.forEach { (apiCode, resFolder) ->
+            downloadFile(
+                apiKey = locoApiKey,
+                apiCode = apiCode,
+                resFolder = resFolder,
+                resDir = resDir
+            )
+        }
     }
 }
+fun downloadFile(apiKey: String, apiCode: String, resFolder: String, resDir: File) {
+    println("-> Descargando [$apiCode] en $resFolder")
+
+    val outputFile = file("$resDir/$resFolder/strings.xml")
+
+    outputFile.parentFile.mkdirs()
+
+    val exportUrl = "https://localise.biz/api/export/locale/$apiCode.xml?key=$apiKey&format=android"
+
+    try {
+        URL(exportUrl).openStream().use { input ->
+            outputFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+        println("✅ Descarga de $apiCode exitosa en $resFolder/strings.xml.")
+    } catch (e: Exception) {
+        println("❌ ERROR al descargar $apiCode. Verifique que la clave '$apiCode' tenga contenido en Localise.biz y que la API Key sea correcta: ${e.message}")
+    }
+}
+
+tasks.named("preBuild").configure {
+    dependsOn("downloadLocoStrings")
+}
+
+
 
 dependencies {
 
@@ -52,6 +135,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.firebase.database)
+    implementation(libs.androidx.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,6 +151,33 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.network)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.google.maps.compose)
+    implementation(libs.google.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation("com.google.accompanist:accompanist-permissions:0.35.2-beta")
+    implementation("com.google.maps.android:maps-compose:4.3.3")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation(libs.grpc.protobuf.lite)
+    implementation(libs.protobuf.kotlin.lite)
+    implementation(libs.grpc.kotlin.stub)
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.ktor:ktor-client-android:3.3.2")
+
+
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.0.2"))
+
+
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+
+    implementation("io.ktor:ktor-client-android:3.0.1")
+    implementation("androidx.compose.material:material-icons-extended")
+
+
+
+    runtimeOnly(libs.grpc.okhttp)
 
     //local bundle room
     implementation(libs.bundles.local)
@@ -79,16 +190,45 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
     implementation(libs.kotlinx.serialization.json)
-    // ---- TESTS INSTRUMENTADOS PARA ROOM MIGRATION ----
+
     androidTestImplementation("androidx.room:room-testing:2.6.1")
-    //androidTestImplementation("androidx.test:core:1.5.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    // Para migración Room en tests instrumentados
+
     androidTestImplementation(libs.room.testing)
 
-// Para ApplicationProvider y demás utilidades de androidTest
+
     androidTestImplementation(libs.androidx.test.core)
 
 
 
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${libs.versions.protobuf.get()}"
+    }
+    plugins {
+        id("java") {
+            artifact = libs.protoc.gen.grpc.java.get().toString()
+        }
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${libs.versions.grpc.get()}"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${libs.versions.grpcKotlin.get()}:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("java") { option("lite") }
+                create("grpc") { option("lite") }
+                create("grpckt") { option("lite") }
+            }
+            it.builtins {
+                create("kotlin") { option("lite") }
+            }
+        }
+    }
 }
