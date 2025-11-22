499ad069212e067f2b855a83ebcd32e54f9fd50c
Sebas <sebastian.terrazas@ucb.edu.bo>
2025-11-21 23:03:40 -0400

    se elimino librerias sin usar

--- app/src/main/java/com/ucb/perritos/features/registroUsuario/data/repository/RegistroUsuarioRepository.kt
+++ app/src/main/java/com/ucb/perritos/features/registroUsuario/data/repository/RegistroUsuarioRepository.kt
@@ -1,9 +1,5 @@
 package com.ucb.perritos.features.registroUsuario.data.repository
 
-import com.ucb.perritos.features.login.domain.model.LoginModel
-import com.ucb.perritos.features.registroMascota.data.datasource.RegistroPerroLocalDataSource
-import com.ucb.perritos.features.registroMascota.domain.model.RegistroPerroModel
-import com.ucb.perritos.features.registroMascota.domain.repository.IRegistroPerroRepository
 import com.ucb.perritos.features.registroUsuario.data.datasource.RegistroUsuarioLocalDataSource
 import com.ucb.perritos.features.registroUsuario.domain.model.UsuarioModel
 import com.ucb.perritos.features.registroUsuario.domain.repository.IRegistroUsuarioRepository
